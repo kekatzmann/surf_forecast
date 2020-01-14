17f484ac5fc6610fafb756e56b15927220ab4361
kekatzmann <katzmannk17@gmail.com>
2020-01-14 10:43:35 -0500

    fixed msw pull

--- surf_app/msw_pull.py
+++ surf_app/msw_pull.py
@@ -1,15 +1,20 @@
 import requests
 
 key = "3d56b802f319631a4a55eea0b642fead"
-sitedata=requests.get("http://magicseaweed.com/api/" + key + "/forecast/?spot_id=382")
+spots = [382,2159]
 
-print(sitedata)
-print(key)
+# pull data from MSW
+sitedata=requests.get("http://magicseaweed.com/api/" + key + "/forecast/?spot_id=" + str(spots[1]))
 
-outfile = open("data.txt", "a") # "a" flag will append lines
+print(sitedata.status_code)
+print(sitedata.json())
 
-outfile.write(sitedata)
-outfile.write("\n")
-outfile.write("\n")
 
-outfile.close()
+# save data somewhere
+
+# outfile = open("data.txt", "a") # "a" flag will append lines
+
+# outfile.write(sitedata.json())
+# outfile.write("\n\n")
+
+# outfile.close()
