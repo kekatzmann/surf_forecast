149a06da342600ad5a1eafa1d758b45dfff2e227
kekatzmann <katzmannk17@gmail.com>
2020-01-26 12:31:34 -0500

    fixed writing to outfile - using status code for now

--- surf_app/msw_pull.py
+++ surf_app/msw_pull.py
@@ -1,4 +1,7 @@
 import requests
+# import pandas as pd
+# from datetime import datetime
+import time
 
 key = "3d56b802f319631a4a55eea0b642fead"
 spots = [382,2159]
@@ -7,14 +10,20 @@ spots = [382,2159]
 sitedata=requests.get("http://magicseaweed.com/api/" + key + "/forecast/?spot_id=" + str(spots[1]))
 
 print(sitedata.status_code)
-print(sitedata.json())
+# print(sitedata.json())
 
+# type(sitedata.json()[0])
+# len(sitedata.json())
+
+# df = pd.DataFrame.from_dict(sitedata.json()[0])
+# df.head()
 
 # save data somewhere
+# now = datetime.now()
+# current_time = now.strftime("%H:%M:%S")
 
-# outfile = open("data.txt", "a") # "a" flag will append lines
+outfile = open("data.txt", "a") # "a" flag will append lines
 
-# outfile.write(sitedata.json())
-# outfile.write("\n\n")
+outfile.write(str(int(time.time())) + ":" + str(sitedata.status_code) + "\n")
 
-# outfile.close()
+outfile.close()
